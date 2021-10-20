635a3079a5042d43368b972945e08c1e5ad28b4b
Tiago-htm <tiagovilela019@gmail.com>
2021-10-20 02:11:46 -0300

    TerminoDaMovimentações
    corrigir o bug do pulo

---
+++ Cachorrinho/Assets/Mecanica/GroudCheck.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{           
+
+    MovPlayer Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject.GetComponent<MovPlayer>();
+    }
+
+    // Update is called once per frame
+        void OnCollisionEnter2D(Collision2D collisor)
+            {
+                    if(collisor.gameObject.layer == 8)
+                    {
+                        Player.isJump = false;
+                    }
+            }
+         void OnCollisionExit2D(Collision2D collisor)
+            { 
+            if(collisor.gameObject.layer == 8)
+           {
+                Player.isJump = true;
+           } 
+
+            }  
+ 
+ }       
