4a6c2f3384a1404c2282662ab9011a3fd48a7950
MatheusCapucho <matheusfcapucho@gmail.com>
2021-10-23 17:29:58 -0300

    Final Build, sem musica

--- Cachorrinho/Assets/Scripts/EndLevel.cs
+++ Cachorrinho/Assets/Scripts/EndLevel.cs
@@ -5,13 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class EndLevel : MonoBehaviour
 {
-
+    [SerializeField] private bool isGoToMenu = false;
     private void OnTriggerEnter2D(Collider2D other)
     {
+
         if (other.gameObject.CompareTag("Player"))
         {
+            if (isGoToMenu)
+            {
+                SceneManager.LoadScene("Menu");
+            } else
+            {
             int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
-            SceneManager.LoadScene(nextScene);
+                SceneManager.LoadScene(nextScene);
+            }
+
         }
     }
 
